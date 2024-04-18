c6c71d6c7a98fd828fb9e78784989e784178761a
Lucien Clin <lucien.clin@uni-tuebingen.de>
2024-04-18 15:22:49 +0200

    Moved patient record Completers here

---
+++ dto_model/src/main/scala/de/dnpm/dip/rd/model/Completers.scala
@@ -0,0 +1,231 @@
+package de.dnpm.dip.rd.model
+
+
+import cats.{
+  Applicative,
+  Id
+}
+import cats.data.NonEmptyList
+import de.dnpm.dip.util.Completer
+import de.dnpm.dip.model.{
+  Patient,
+  Observation,
+  Site
+}
+import de.dnpm.dip.coding.{
+  Code,
+  Coding,
+  CodeSystem,
+  CodeSystemProvider
+}
+import de.dnpm.dip.coding.hgnc.HGNC
+import de.dnpm.dip.coding.hgvs.HGVS
+import de.dnpm.dip.coding.icd.ICD10GM
+import de.dnpm.dip.rd.model.{
+  ACMG,
+  HPO,
+  HPOTerm,
+  OMIM,
+  Orphanet,
+  RDDiagnosis,
+  RDNGSReport,
+  RDPatientRecord,
+  SmallVariant,
+  CopyNumberVariant,
+  StructuralVariant
+}
+import shapeless.{
+  Coproduct,
+  :+:,
+  CNil
+}
+
+
+trait Completers
+{
+
+  import Completer.syntax._
+  import scala.util.chaining._ 
+
+
+  protected implicit val hpOntology: CodeSystem[HPO]
+
+  protected implicit val hgnc: CodeSystem[HGNC]
+
+  protected implicit val ordo: CodeSystem[Orphanet]
+
+  protected implicit val omim: CodeSystem[OMIM]
+
+  protected implicit val icd10gm: CodeSystemProvider[ICD10GM,Id,Applicative[Id]]
+
+
+  implicit val patientCompleter: Completer[Patient] =
+    Completer.of(
+      pat =>
+        pat.copy(
+          gender       = pat.gender.complete,
+          managingSite = Some(Site.local)
+        )
+    )
+
+
+  implicit def coproductCodingCompleter[
+    H: Coding.System,
+    T <: Coproduct
+  ](
+    implicit
+    compH: Completer[Coding[H]],
+    compT: Completer[Coding[T]]
+  ): Completer[Coding[H :+: T]] =
+    Completer.of { 
+      coding =>
+        (
+          if (coding.system == Coding.System[H].uri)
+            compH(coding.asInstanceOf[Coding[H]])
+          else
+            compT(coding.asInstanceOf[Coding[T]])
+        )
+        .asInstanceOf[Coding[H :+: T]]
+    }
+
+  implicit def terminalCoproductCodingCompleter[
+    H: Coding.System
+  ](
+    implicit
+    compH: Completer[Coding[H]],
+  ): Completer[Coding[H :+: CNil]] =
+    compH.asInstanceOf[Completer[Coding[H :+: CNil]]]
+
+
+
+  implicit val diagnosisCompleter: Completer[RDDiagnosis] =
+    Completer.of(
+      diag =>
+        diag.copy(
+          categories = diag.categories.complete, 
+          status     = diag.status.complete
+        )
+    )
+
+
+  implicit val hpoTermCompleter: Completer[HPOTerm] =
+    Completer.of(
+      hpo =>
+        hpo.copy(
+          value = hpo.value.complete
+        )
+    )
+
+
+  implicit val acmgCriterionCompleter: Completer[ACMG.Criterion] =
+    Completer.of(
+      acmg =>
+        acmg.copy(
+          value    = acmg.value.complete,
+          modifier = acmg.modifier.complete
+        )
+    )
+
+
+  implicit val smallVariantCompleter: Completer[SmallVariant] =
+    Completer.of(
+      v =>
+        v.copy(
+          genes               = v.genes.complete,
+          acmgClass           = v.acmgClass.complete,
+          acmgCriteria        = v.acmgCriteria.complete,
+          zygosity            = v.zygosity.complete,
+          segregationAnalysis = v.segregationAnalysis.complete,
+          modeOfInheritance   = v.modeOfInheritance.complete,
+          significance        = v.significance.complete,
+        )
+    )
+
+
+  implicit val structuralVariantCompleter: Completer[StructuralVariant] =
+    Completer.of(
+      v =>
+        v.copy(
+          genes               = v.genes.complete,
+          acmgClass           = v.acmgClass.complete,
+          acmgCriteria        = v.acmgCriteria.complete,
+          zygosity            = v.zygosity.complete,
+          segregationAnalysis = v.segregationAnalysis.complete,
+          modeOfInheritance   = v.modeOfInheritance.complete,
+          significance        = v.significance.complete,
+        )
+    )
+
+
+  implicit val copyNumberVariantCompleter: Completer[CopyNumberVariant] =
+    Completer.of(
+      v =>
+        v.copy(
+          genes               = v.genes.complete,
+          acmgClass           = v.acmgClass.complete,
+          acmgCriteria        = v.acmgCriteria.complete,
+          zygosity            = v.zygosity.complete,
+          segregationAnalysis = v.segregationAnalysis.complete,
+          modeOfInheritance   = v.modeOfInheritance.complete,
+          significance        = v.significance.complete,
+        )
+    )
+
+
+  implicit val ngsReportCompleter: Completer[RDNGSReport] =
+    Completer.of(
+      ngs =>
+        ngs.copy(
+          smallVariants      = ngs.smallVariants.complete,
+          structuralVariants = ngs.structuralVariants.complete,
+          copyNumberVariants = ngs.copyNumberVariants.complete
+        )
+    )
+
+
+  implicit val rdPatientRecordCompleter: Completer[RDPatientRecord] =
+    Completer.of(
+      patRec =>
+        patRec.copy(
+          patient    = patRec.patient.complete,
+          diagnosis  = patRec.diagnosis.complete,
+          hpoTerms   = patRec.hpoTerms.complete,
+          ngsReports = patRec.ngsReports.complete
+        )
+    )
+
+}
+
+object Completers extends Completers
+{
+
+  override implicit lazy val hpOntology: CodeSystem[HPO] =
+    HPO.Ontology
+      .getInstance[cats.Id]
+      .get
+      .latest
+
+  override implicit lazy val hgnc: CodeSystem[HGNC] =
+    HGNC.GeneSet
+      .getInstance[cats.Id]
+      .get
+      .latest
+
+  override implicit lazy val ordo: CodeSystem[Orphanet] =
+    Orphanet.Ordo
+      .getInstance[cats.Id]
+      .get
+      .latest
+
+  override implicit lazy val omim: CodeSystem[OMIM] =
+    OMIM.Catalog
+      .getInstance[cats.Id]
+      .get
+      .latest
+
+  override implicit lazy val icd10gm: CodeSystemProvider[ICD10GM,Id,Applicative[Id]] =
+    ICD10GM.Catalogs
+      .getInstance[cats.Id]
+      .get
+
+}
